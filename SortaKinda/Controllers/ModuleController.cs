d2c4f0111532cb41092ca7c04ce92e574b8eb98a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-12-27 15:18:40 -0800

    Generate sorting events for both source and target inventory

--- SortaKinda/Controllers/ModuleController.cs
+++ SortaKinda/Controllers/ModuleController.cs
@@ -52,28 +52,70 @@ public class ModuleController : IDisposable {
         if (!Service.ClientState.IsLoggedIn) return;
         
         foreach (var module in modules) {
-            var inventoryTypes = events
-                .Where(itemEvent => itemEvent.Type switch {
-                    GameInventoryEvent.Added when SortaKindaController.SystemConfig.SortOnItemAdded => true,
-                    GameInventoryEvent.Removed when SortaKindaController.SystemConfig.SortOnItemRemoved => true,
-                    GameInventoryEvent.Changed when SortaKindaController.SystemConfig.SortOnItemChanged => true,
-                    GameInventoryEvent.Moved when SortaKindaController.SystemConfig.SortOnItemMoved => true,
-                    GameInventoryEvent.Split when SortaKindaController.SystemConfig.SortOnItemSplit => true,
-                    GameInventoryEvent.Merged when SortaKindaController.SystemConfig.SortOnItemMerged => true,
-                    _ => false
-                })
-                .Select(itemEvent => (InventoryType) itemEvent.Item.ContainerType)
-                .Where(inventoryType => module.InventoryTypes.Contains(inventoryType))
-                .Distinct()
-                .ToArray();
+            var inventoryTypes = new HashSet<InventoryType>();
+
+            foreach (var itemEvent in events) {
+                if (!IsEventAllowed(itemEvent)) continue;
+
+                AddChangedInventories(itemEvent, inventoryTypes);
+
+                inventoryTypes.RemoveWhere(type => !module.InventoryTypes.Contains(type));
+            }
 
             if (inventoryTypes.Any()) {
-                module.InventoryChanged(inventoryTypes);
+                module.InventoryChanged(inventoryTypes.ToArray());
             }
+        }
+    }
+
+    private static void AddChangedInventories(InventoryEventArgs itemEvent, ICollection<InventoryType> inventoryTypes) {
+        switch (itemEvent) {
+            case null:
+                break;
+
+            case InventoryItemAddedArgs itemAdded:
+                inventoryTypes.Add((InventoryType) itemAdded.Inventory);
+                break;
+            
+            case InventoryItemRemovedArgs itemRemoved:
+                inventoryTypes.Add((InventoryType) itemRemoved.Inventory);
+                break;
+
+            case InventoryItemChangedArgs itemChanged:
+                inventoryTypes.Add((InventoryType) itemChanged.Inventory);
+                break;
+
+            case InventoryItemMergedArgs itemMerged:
+                inventoryTypes.Add((InventoryType) itemMerged.SourceInventory);
+                inventoryTypes.Add((InventoryType) itemMerged.TargetInventory);
+                break;
+
+            case InventoryItemMovedArgs itemMoved:
+                inventoryTypes.Add((InventoryType) itemMoved.SourceInventory);
+                inventoryTypes.Add((InventoryType) itemMoved.TargetInventory);
+                break;
+
+            case InventoryItemSplitArgs itemSplit:
+                inventoryTypes.Add((InventoryType) itemSplit.SourceInventory);
+                inventoryTypes.Add((InventoryType) itemSplit.TargetInventory);
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(itemEvent), itemEvent, null);
         }
     }
 
     public void DrawModule(ModuleName module) {
         modules.FirstOrDefault(drawableModule => drawableModule.ModuleName == module)?.Draw();
     }
+
+    private static bool IsEventAllowed(InventoryEventArgs arg) => arg.Type switch {
+        GameInventoryEvent.Added when SortaKindaController.SystemConfig.SortOnItemAdded => true,
+        GameInventoryEvent.Removed when SortaKindaController.SystemConfig.SortOnItemRemoved => true,
+        GameInventoryEvent.Changed when SortaKindaController.SystemConfig.SortOnItemChanged => true,
+        GameInventoryEvent.Moved when SortaKindaController.SystemConfig.SortOnItemMoved => true,
+        GameInventoryEvent.Split when SortaKindaController.SystemConfig.SortOnItemSplit => true,
+        GameInventoryEvent.Merged when SortaKindaController.SystemConfig.SortOnItemMerged => true,
+        _ => false
+    };
 }
