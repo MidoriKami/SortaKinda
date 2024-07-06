8212b8e3568094b976480363e8562d5c3b1c6fb7
MidoriKami <mi_kami@outlook.com>
2024-07-05 21:26:09 -0700

    Name Standardization

--- SortaKinda/Controllers/AreaPaintController.cs
+++ SortaKinda/Controllers/AreaPaintController.cs
@@ -8,16 +8,16 @@ using SortaKinda.Windows;
 namespace SortaKinda.Controllers;
 
 public class AreaPaintController {
-    private static bool _dragStarted;
-    private static Vector2 _dragStartPosition = Vector2.Zero;
-    private static Vector2 _dragStopPosition = Vector2.Zero;
+    private static bool dragStarted;
+    private static Vector2 dragStartPosition = Vector2.Zero;
+    private static Vector2 dragStopPosition = Vector2.Zero;
     private Vector2 lastWindowPosition = Vector2.Zero;
 
     public static Rectangle GetDragBounds() {
-        if (_dragStarted is false) return Rectangle.Empty;
+        if (dragStarted is false) return Rectangle.Empty;
 
-        var startPoint = new Point((int) _dragStartPosition.X, (int) _dragStartPosition.Y);
-        var stopPoint = new Point((int) _dragStopPosition.X, (int) _dragStopPosition.Y);
+        var startPoint = new Point((int) dragStartPosition.X, (int) dragStartPosition.Y);
+        var stopPoint = new Point((int) dragStopPosition.X, (int) dragStopPosition.Y);
         var size = new Size(Math.Abs(stopPoint.X - startPoint.X), Math.Abs(stopPoint.Y - startPoint.Y));
 
         var minX = Math.Min(startPoint.X, stopPoint.X);
@@ -28,25 +28,25 @@ public class AreaPaintController {
 
     public void Draw() {
         if (!ShouldDraw()) {
-            _dragStarted = false;
+            dragStarted = false;
             return;
         }
 
-        if (ImGui.IsMouseClicked(ImGuiMouseButton.Left) && !_dragStarted) {
-            _dragStartPosition = ImGui.GetMousePos();
-            _dragStopPosition = ImGui.GetMousePos();
-            _dragStarted = true;
+        if (ImGui.IsMouseClicked(ImGuiMouseButton.Left) && !dragStarted) {
+            dragStartPosition = ImGui.GetMousePos();
+            dragStopPosition = ImGui.GetMousePos();
+            dragStarted = true;
         }
 
-        if (ImGui.IsMouseDragging(ImGuiMouseButton.Left) && _dragStarted) {
-            _dragStopPosition = ImGui.GetMousePos();
+        if (ImGui.IsMouseDragging(ImGuiMouseButton.Left) && dragStarted) {
+            dragStopPosition = ImGui.GetMousePos();
 
             if (SortaKindaController.SortController.SelectedRule is not { Color: var ruleColor }) return;
-            ImGui.GetWindowDrawList().AddRect(_dragStartPosition, _dragStopPosition, ImGui.GetColorU32(ruleColor), 0.0f, ImDrawFlags.None, 3.0f);
-            ImGui.GetWindowDrawList().AddRectFilled(_dragStartPosition, _dragStopPosition, ImGui.GetColorU32(ruleColor with { W = 0.33f }));
+            ImGui.GetWindowDrawList().AddRect(dragStartPosition, dragStopPosition, ImGui.GetColorU32(ruleColor), 0.0f, ImDrawFlags.None, 3.0f);
+            ImGui.GetWindowDrawList().AddRectFilled(dragStartPosition, dragStopPosition, ImGui.GetColorU32(ruleColor with { W = 0.33f }));
         }
         else if (ImGui.IsMouseReleased(ImGuiMouseButton.Left)) {
-            _dragStarted = false;
+            dragStarted = false;
         }
     }
 
