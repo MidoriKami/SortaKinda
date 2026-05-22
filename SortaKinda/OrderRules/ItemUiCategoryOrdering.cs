7dc93384370af1626d84fd45d1edef5dfb43d22f
MidoriKami <mi_kami@outlook.com>
2026-05-22 13:36:54 -0700

    Add ItemUiCategoryOrdering.cs

---
+++ SortaKinda/OrderRules/ItemUiCategoryOrdering.cs
@@ -0,0 +1,17 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Game;
+
+namespace SortaKinda.OrderRules;
+
+public class ItemUiCategoryOrdering : OrderingRuleBase {
+	public override string Label
+		=> "Item Ui Category";
+
+	protected override string NotReversedLabel
+		=> "Low";
+
+	protected override string ReversedLabel
+		=> "High";
+
+	public override unsafe int Compare(InventoryItem* left, InventoryItem* right)
+		=> left->UiCategory.RowId.CompareTo(right->UiCategory.RowId);
+}
